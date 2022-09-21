9a17409e50caa7ed6fe9ca6eec2be12fb649daae
Danielle Rodrigues <31356718+DanielleRodrigues@users.noreply.github.com>
2022-09-21 09:05:56 -0300

    Create script.ts

---
+++ atividade4/script.ts
@@ -0,0 +1,6 @@
+/*ATIVIDADE 4
+Crie uma classe funcionário e apresente os atributos e métodos referentes esta classe, 
+em seguida crie um objeto funcionário, defina as instâncias deste objeto e apresente as informações deste objeto no console.*/
+
+
+
